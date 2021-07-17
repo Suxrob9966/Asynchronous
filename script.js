b11e9b83baeac9bd1b5c762703d9ef7377a07a60
Suxrob9966 <rabbimovich9966@gmail.com>
2021-07-17 11:09:34 -0500

    First AJAX call

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 /////////////////////////////////////// 
+
+// First AJAX call
+const request = new XMLHttpRequest();
+// open takes 2 params: second is the URL 
+request.open('GET', 'https://restcountries.eu/rest/v2/name/uzbekistan');
+// send the request
+request.send();
+
+// we call callback function as soon as requst is loaded, we use eventlistener
+request.addEventListener('load', function(){
+    // the result is in JSON format
+    // console.log(this.responseText);
+    // we need to convert it JS object 
+    const data = JSON.parse(this.responseText);
+    console.log(data);
+})
